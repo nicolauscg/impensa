439af0226970993859a8df0949c74124ed76cfdc
Nicolaus Christian Gozali <nicolauscg@gmail.com>
2020-03-27 18:18:31 +1000

    configure cors

--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 	"github.com/nicolauscg/impensa/controllers"
 	handlerPkg "github.com/nicolauscg/impensa/handlers"
 )
@@ -12,7 +13,19 @@ func init() {
 		panic(err)
 	}
 
+	allowedOrigins := make([]string, 0)
+	if beego.BConfig.RunMode == "dev" {
+		allowedOrigins = append(allowedOrigins, "http://localhost:3000")
+	}
+
 	ns := beego.NewNamespace("v1",
+		beego.NSBefore(cors.Allow(&cors.Options{
+			AllowOrigins:     allowedOrigins,
+			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+			AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Content-Type"},
+			ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
+			AllowCredentials: true,
+		})),
 		beego.NSNamespace("/auth",
 			beego.NSInclude(
 				&controllers.AuthController{Handler: handler},
