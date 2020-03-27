7d80822522b31054f989ec716c4a02470cf73210
Nicolaus Christian Gozali <nicolauscg@gmail.com>
2020-03-27 18:18:15 +1000

    fix handler return error when db not found, remove use of sync.Once

--- handlers/handler.go
+++ handlers/handler.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/astaxie/beego"
@@ -12,12 +11,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
-)
-
-var (
-	once    sync.Once
-	handler *Handler
-	err     error
 )
 
 type Handler struct {
@@ -30,32 +23,35 @@ type Entity struct {
 	Transaction models.TransactionOrmer
 }
 
-func NewHandler(databaseName string) (*Handler, error) {
-	once.Do(func() {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-		if err != nil {
-			err = errors.New(fmt.Sprintf("[handler/handler] mongo.Connect error. %+v", err))
-			return
-		}
+func NewHandler(databaseName string) (handler *Handler, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		err = errors.New(fmt.Sprintf("[handler/handler] mongo.Connect error. %+v", err))
+		return
+	}
 
-		ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
-		defer cancel()
-		// check if MongoDB server found
-		err = client.Ping(ctx, readpref.Primary())
-		if err != nil {
-			err = errors.New(fmt.Sprintf("[handler/handler] mongo client ping error. %+v", err))
-			return
-		}
+	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	// check if MongoDB server found
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		err = errors.New(fmt.Sprintf("[handler/handler] mongo client ping error. %+v", err))
+		return
+	}
 
-		handler = &Handler{db: client.Database(databaseName)}
-		handler.Orms = &Entity{
-			models.NewUserOrm(handler.db),
-			models.NewTransactionOrm(handler.db),
-		}
+	handler = &Handler{db: client.Database(databaseName)}
+	handler.Orms = &Entity{
+		models.NewUserOrm(handler.db),
+		models.NewTransactionOrm(handler.db),
+	}
+
+	if handler == nil {
+		err = errors.New("[handler/handler] database not found. %+v")
+	} else {
 		beego.Info("[handler/handler] database successfully connected.")
-	})
+	}
 
 	return handler, err
 }
