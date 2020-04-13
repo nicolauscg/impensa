62ed113df638a4dceda4af01da6ddb11aeee6030
Nicolaus Christian Gozali <nicolauscg@gmail.com>
2020-04-13 19:04:56 +1000

    remove quiet and add verbose to scp

--- scripts/init_deploy.sh
+++ scripts/init_deploy.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 echo "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-scp -q ./docker-compose.yml ./scripts/restart_service.sh $HOST:~/
-scp -q -r ./nginx-conf $HOST:~/
+scp -v ./docker-compose.yml ./scripts/restart_service.sh $HOST:~/
+scp -v -r ./nginx-conf $HOST:~/
 
 echo "init_deploy.sh finished"
