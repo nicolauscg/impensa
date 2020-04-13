95bf3dc3d97311172df5de0645ae1a6834b25a04
Nicolaus Christian Gozali <nicolauscg@gmail.com>
2020-04-13 19:19:18 +1000

    change command to disable strict host checking in deploy script

--- scripts/init_deploy.sh
+++ scripts/init_deploy.sh
@@ -5,7 +5,7 @@ chmod 400 $DEPLOYMENT_KEY
 ssh-add $DEPLOYMENT_KEY
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-echo "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
 scp -v ./docker-compose.yml ./scripts/restart_service.sh $HOST:~/
 scp -v -r ./nginx-conf $HOST:~/
