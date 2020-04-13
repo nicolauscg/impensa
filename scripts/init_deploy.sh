03a7a157bac6a420f25d79ee65f7bcebebab1d10
Nicolaus Christian Gozali <nicolauscg@gmail.com>
2020-04-13 18:41:41 +1000

    set jobs to run manually, fix deploy script

--- scripts/init_deploy.sh
+++ scripts/init_deploy.sh
@@ -3,9 +3,9 @@ which ssh-agent || ( apk add --update openssh-client )
 eval $(ssh-agent -s)
 chmod 400 $DEPLOYMENT_KEY
 ssh-add $DEPLOYMENT_KEY
-echo "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
+echo "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 scp -q ./docker-compose.yml ./scripts/restart_service.sh $HOST:~/
 scp -q -r ./nginx-conf $HOST:~/
