5d4bbe11b933390b0480bb3f02a47cdba51e99fb
Nicolaus Christian Gozali <nicolauscg@gmail.com>
2020-05-21 23:32:24 +1000

    [FE] implement monthly view in graph page

--- frontend/src/containers/GraphPage/index.js
+++ frontend/src/containers/GraphPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
 import {
   useAxiosSafely,
@@ -6,7 +6,28 @@ import {
   urlGraphTransactionAccount
 } from "../../api";
 
+import { Typography, IconButton } from "@material-ui/core";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
+
+const moment = require("moment");
+
 export default function GraphPage() {
+  const [currentMonthViewed, setMonthViewed] = useState(
+    moment().startOf("month")
+  );
+  const setNextMonth = () =>
+    setMonthViewed(currentMonthViewed.clone().add(1, "month"));
+  const setPreviousMonth = () =>
+    setMonthViewed(currentMonthViewed.clone().subtract(1, "month"));
+  const getStartOfCurrentMonthAsString = () =>
+    currentMonthViewed.format("YYYY-MM-DD");
+  const getEndOfCurrentMonthAsString = () =>
+    currentMonthViewed
+      .clone()
+      .endOf("month")
+      .format("YYYY-MM-DD");
+
   const [
     { data: categoryGraphData },
     fetchGraphTransactionCategory
@@ -15,10 +36,21 @@ export default function GraphPage() {
     { data: accountGraphData },
     fetchGraphTransactionAccount
   ] = useAxiosSafely(urlGraphTransactionAccount());
+
   useEffect(() => {
-    fetchGraphTransactionCategory();
-    fetchGraphTransactionAccount();
-  }, []);
+    fetchGraphTransactionCategory({
+      params: {
+        dateTimeStart: getStartOfCurrentMonthAsString(),
+        dateTimeEnd: getEndOfCurrentMonthAsString()
+      }
+    });
+    fetchGraphTransactionAccount({
+      params: {
+        dateTimeStart: getStartOfCurrentMonthAsString(),
+        dateTimeEnd: getEndOfCurrentMonthAsString()
+      }
+    });
+  }, [currentMonthViewed]);
   const formattedCategoryGraphData = [["Category", "Amount"]].concat(
     categoryGraphData.map(pieChartSliceData => [
       pieChartSliceData.label,
@@ -48,6 +80,15 @@ export default function GraphPage() {
   return (
     <div>
       <h1>Graph</h1>
+      <div className="d-flex flex-row justify-content-center align-items-center">
+        <IconButton className="mr-3" onClick={setPreviousMonth}>
+          <KeyboardArrowLeftIcon fontSize="large" />
+        </IconButton>
+        <Typography>{currentMonthViewed.format("MMM YYYY")}</Typography>
+        <IconButton className="ml-3" onClick={setNextMonth}>
+          <KeyboardArrowRightIcon fontSize="large" />
+        </IconButton>
+      </div>
       <Chart
         chartType="PieChart"
         width="100%"
