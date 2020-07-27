03d78afe3ac9e1962c08326e263bb2a5a8841807
Nicolaus Christian Gozali <nicolauscg@gmail.com>
2020-07-27 22:39:28 +1000

    add folder path var to deployment

--- scripts/init_backend.sh
+++ scripts/init_backend.sh
@@ -2,8 +2,7 @@ cp $BACKEND_ENV ./.env
 cp $API_SECRET ./impensa_be_api_secret.txt
 cp $DB_CONN_STRING ./impensa_be_mgoconnstring.txt
 cp $MAILGUN_API ./impensa_be_mailgun_api.txt
-cp $MAILGUN_API ./impensa_be_mailgun_api.txt
 cp $GOOGLE_OAUTH_CLIENT_ID ./impensa_be_google_oauth_client_id.txt
 cp $GOOGLE_OAUTH_CLIENT_SECRET ./impensa_be_google_oauth_client_secret.txt
-scp ./.env ./impensa_be_api_secret.txt ./impensa_be_mgoconnstring.txt ./impensa_be_mailgun_api.txt ./impensa_be_google_oauth_client_id.txt ./impensa_be_google_oauth_client_secret.txt $HOST:~/
+scp ./.env ./impensa_be_api_secret.txt ./impensa_be_mgoconnstring.txt ./impensa_be_mailgun_api.txt ./impensa_be_google_oauth_client_id.txt ./impensa_be_google_oauth_client_secret.txt $HOST:$FOLDER_PATH
 echo "init_backend.sh finished"
--- scripts/init_deploy.sh
+++ scripts/init_deploy.sh
@@ -7,8 +7,7 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
-scp ./docker-compose.yml ./scripts/restart_service.sh $HOST:~/
-scp -r ./nginx-conf $HOST:~/
+scp ./docker-compose.yml ./scripts/restart_service.sh $HOST:$FOLDER_PATH
 
 echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
 
